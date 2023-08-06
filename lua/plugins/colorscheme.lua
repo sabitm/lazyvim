09a6c3167e828ef55dd1d01035eddae955980efd
sabitm <sabitmaulanaa@gmail.com>
2023-08-06 16:48:51 +0700

    change colorscheme to gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
   -- configure LazyVim to load catpuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox",
     },
-  }
+  },
 }
