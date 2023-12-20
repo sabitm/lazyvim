12e2c75cfab27c62e461b783f2d57acea09ad5f6
sabitm <sabitmaulanaa@gmail.com>
2023-12-20 12:17:54 +0700

    Add svelte lsp

--- lua/plugins/svelte.lua
+++ lua/plugins/svelte.lua
@@ -1,10 +1,20 @@
 return {
+  -- add svelte grammar
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "css",
         "svelte",
+      })
+    end,
+  },
+  -- add svelte lsp
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "svelte-language-server",
       })
     end,
   },
