ff5fc9b8c1201e5de5f9536521d13f0ddfcafaee
sabitm <sabitmaulanaa@gmail.com>
2024-05-11 09:30:25 +0700

    Change formatter to ruff for python

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -4,10 +4,18 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "black",
         "ruff",
         "ruff-lsp",
       })
     end,
+  },
+  -- change formatter
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "ruff_format" },
+      },
+    },
   },
 }
