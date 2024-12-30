39c30fe48556f04033aaad341a1412e56c196239
sabitm <sabitmaulanaa@gmail.com>
2024-12-30 13:02:02 +0700

    Move back to codeium (copilot crash)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
