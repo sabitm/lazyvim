945ae74465c660dfbea0c6188e3d2784612ae6a4
sabitm <sabitmaulanaa@gmail.com>
2025-09-30 22:52:52 +0700

    Use gruvbox colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- configure LazyVim to load below theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
